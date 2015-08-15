b71bfb785b8dbbb5adcbf3e2853e79570d611b9e
Alex Gomes <alexgomes@Alexs-MacBook-Air.local>
2015-08-15 19:52:58 -0400

    First upload | with action bar done

---
+++ app/src/main/java/com/example/realtalk/realtalk/FontManager.java
@@ -0,0 +1,10 @@
+package com.example.realtalk.realtalk;
+
+/**
+ * Created by alexgomes on 2015-08-15.
+ */
+public class FontManager {
+
+
+
+}
---
+++ app/src/main/java/com/example/realtalk/realtalk/first_screen.java
@@ -0,0 +1,32 @@
+package com.example.realtalk.realtalk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class first_screen extends AppCompatActivity{
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_screen);
+
+        toolbar = (Toolbar) findViewById(R.id.custom_actionbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ImageButton logo = (ImageButton)findViewById(R.id.logo);
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "You clicked the logo", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
+
