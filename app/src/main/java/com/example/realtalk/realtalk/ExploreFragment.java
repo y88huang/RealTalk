455105cb9ebd59904506e2b0b827f1a7f6cb91fe
Alex Gomes <alex_09hg@yahoo.com>
2015-10-05 15:49:33 -0400

    explore icon adding

--- app/src/main/java/com/example/realtalk/realtalk/ExploreFragment.java
+++ app/src/main/java/com/example/realtalk/realtalk/ExploreFragment.java
@@ -1,13 +1,24 @@
 package com.example.realtalk.realtalk;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.StringBuilderPrinter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class ExploreFragment extends android.support.v4.app.Fragment {
@@ -36,5 +47,64 @@ public class ExploreFragment extends android.support.v4.app.Fragment {
                 getActivity().getSupportFragmentManager().beginTransaction().remove(ExploreFragment.this).commit();
             }
         });
+
+        GridView gridview = (GridView) getActivity().findViewById(R.id.exploreGridView);
+        ArrayList<String> item = new ArrayList<String>();
+        for (int i = 0; i < 50; i++) {
+            item.add("HELLO"+i);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_expandable_list_item_1,item);
+
+        gridview.setAdapter(adapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "Grid" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+class GridAdapter extends BaseAdapter{
+
+    private Context context;
+    private String[] itemText;
+    private int[] image;
+    private static LayoutInflater inflater = null;
+
+    public GridAdapter(Context context,String[] text,int[] image){
+        this.context = context;
+        this.itemText = text;
+        this.image = image;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Holder holder = new Holder();
+        convertView = inflater.inflate(R.layout.explore_single_grid_item,null);
+        holder.icon = (TextView)convertVi;
+        return convertView;
+    }
+    public class Holder{
+        TextView itemName;
+        ImageView icon;
     }
 }
