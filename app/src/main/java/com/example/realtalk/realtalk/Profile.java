c0ddb074e4059547be235872f123b59af178961d
Alex Gomes <alex_09hg@yahoo.com>
2015-10-09 11:06:43 -0400

    Swipe cards fixed

--- app/src/main/java/com/example/realtalk/realtalk/Profile.java
+++ app/src/main/java/com/example/realtalk/realtalk/Profile.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -61,15 +62,10 @@ public class Profile extends AppCompatActivity {
         btnSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                PreferenceFragment settingsFragment = new SettingsFragment();
-//                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-////                transaction.setCustomAnimations(R.anim.abc_slide_in_bottom,R.anim.abc_slide_out_bottom);
-//                transaction.replace(android.R.id.content,settingsFragment);
-//                transaction.addToBackStack(null);
-//                transaction.commit();
-
-//                getFragmentManager().beginTransaction().replace(android.R.id.content,
-//                        new SettingsFragment()).commit(); 
+                Settings settingsFragment = new Settings();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.add(android.R.id.content,settingsFragment);
+                fragmentTransaction.commit();
             }
         });
 
---
+++ app/src/main/java/com/example/realtalk/realtalk/Settings.java
@@ -0,0 +1,41 @@
+package com.example.realtalk.realtalk;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by alexgomes on 2015-09-29. - alex.09hg@gmail.com
+ */
+public class Settings extends Fragment {
+
+    TextView txtSettings;
+    ImageButton btnBack;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_screen, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        txtSettings = (TextView) getActivity().findViewById(R.id.txtSettings);
+        txtSettings.setTypeface(FontManager.setFont(getActivity(), FontManager.Font.JustAnotherHandRegular));
+
+        btnBack = (ImageButton) getActivity().findViewById(R.id.btnBackButton);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+    }
+}
