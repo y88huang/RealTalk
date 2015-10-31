1eed9277950d70eb85a1e3f2efbef3a73346a19b
Alex Gomes <alex_09hg@yahoo.com>
2015-10-31 13:12:34 -0400

    splashscreen only running once

--- app/src/main/java/com/example/realtalk/realtalk/SplashScreen.java
+++ app/src/main/java/com/example/realtalk/realtalk/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.realtalk.realtalk;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -12,25 +14,38 @@ import static com.example.realtalk.realtalk.Utility.isNetworkStatusAvailable;
 public class SplashScreen extends Activity {
 
     private static int SPLASH_TIME_OUT = 3000;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        sharedPreferences = getSharedPreferences(getString(R.string.tlpSharedPreference), 0);
+        final boolean firstRun = sharedPreferences.getBoolean("firstRun",true);
+
         if (!isNetworkStatusAvailable(SplashScreen.this)) {
             Utility.KillApplicationDialog(getString(R.string.connectionError), SplashScreen.this);
         }else{
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
+            if(firstRun){
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        FirstOnBoarding firstOnBoarding = new FirstOnBoarding();
+                        android.app.FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                        transaction.add(android.R.id.content, firstOnBoarding);
+                        transaction.commit();
 
-                    FirstOnBoarding firstOnBoarding = new FirstOnBoarding();
-                    android.app.FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                    transaction.add(android.R.id.content, firstOnBoarding);
-                    transaction.commit();
-                }
-            }, SPLASH_TIME_OUT);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putBoolean("firstRun", false);
+                        editor.apply();
+                    }
+                }, SPLASH_TIME_OUT);
+            }else{
+                Intent runHomeScreen = new Intent(this,HomeScreen.class);
+                startActivity(runHomeScreen);
+            }
+
         }
 
 
